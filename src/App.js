c51c13296d14c83e4b7e4a68289df6138f2d1774
Alexander Weinbeck <aweinbeck@outlook.com>
2022-03-07 20:20:55 +0100

    added remarks

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ class LambdaDemo extends Component {
 			loading: true,
 			initialTrackTimes: [],
 			trackTimes: [],
+			lrAmounts: [],
 			selectedClass: 'S',
 			selectedTrack: 'BBD Breeze'
 		};
@@ -21,7 +22,25 @@ class LambdaDemo extends Component {
 			.then(response => response.json())
 			.then(json => this.setState({loading: false, initialTrackTimes: json}))
 			.then(() => this.handleClassSelection(this.state.selectedClass))
-			.then(() => this.handleTrackSelection(this.state.selectedTrack));
+			.then(() => this.handleTrackSelection(this.state.selectedTrack))
+			.then(() => {
+				//calc who has most LRs
+				const lrTimes = [];
+
+				for(const lrTime of this.state.initialTrackTimes.filter((tt) => tt.rank === "1")){
+					const lrFoundIndex = lrTimes.findIndex(lr => lr.time.alias === lrTime.alias);
+					if(lrFoundIndex === -1){
+						lrTimes.push({
+							time: lrTime,
+							amount: 1,
+						})
+					} else {
+						lrTimes[lrFoundIndex].amount++;
+					}
+				}
+				this.setState({ lrAmounts: lrTimes.filter(el => el.time.alias).sort((a, b) => b.amount - a.amount) });
+				//calc who has most Top10 appearances
+			});
 
 	}
 
@@ -78,7 +97,7 @@ class LambdaDemo extends Component {
 							<Col>
 								<DropdownButton	id="dropdown-class-track" title={this.state.selectedTrack.toString()}>
 									{
-										[...new Set(this.state.initialTrackTimes.map(tt => tt.track))].filter(e => e).map((track, i) =>
+										[...new Set(this.state.initialTrackTimes.map(tt => tt.track))].filter(e => e).sort().map((track, i) =>
 											<Dropdown.Item key={'track' + i} style={{fontSize: "12px", maxHeight: "20px"}} onClick={e => this.handleTrackSelection(track)}>{track}</Dropdown.Item>
 										)
 									}
@@ -90,7 +109,7 @@ class LambdaDemo extends Component {
 							<Col>
 								<DropdownButton id="dropdown-class-class" title={this.state.selectedClass.toString()}>
 									{
-										[...new Set(this.state.initialTrackTimes.map(tt => tt.class))].filter(e => e).map((trackClass, i) =>
+										[...new Set(this.state.initialTrackTimes.map(tt => tt.class))].filter(e => e).sort().map((trackClass, i) =>
 											<Dropdown.Item key={'class' + i} onClick={e => this.handleClassSelection(trackClass)}>{trackClass}</Dropdown.Item>
 										)
 									}
@@ -121,15 +140,45 @@ class LambdaDemo extends Component {
 									</tr>)
 							}
 						</table>
+						{/*<h2>*/}
+						{/*	Amount of Lap Records per Alias*/}
+						{/*</h2>*/}
+						{/*<table>*/}
+						{/*	<tr>*/}
+						{/*		/!*<th>Track</th>*!/*/}
+						{/*		/!*<th>Class</th>*!/*/}
+						{/*		<th>Rank</th>*/}
+						{/*		<th>Alias</th>*/}
+						{/*		<th>Amount</th>*/}
+						{/*		/!*<th>Time</th>*!/*/}
+						{/*		<th>Twitch</th>*/}
+						{/*	</tr>*/}
+
+						{/*	{*/}
+						{/*		this.state.lrAmounts.slice(0, 10).map((tt, i) =>*/}
+						{/*			<tr key={i}>*/}
+						{/*				/!*<td>{tt.track}</td>*!/*/}
+						{/*				/!*<td>{tt.class}</td>*!/*/}
+						{/*				<td>{i + 1}</td>*/}
+						{/*				<td>{tt.time.alias}</td>*/}
+						{/*				<td>{tt.amount}</td>*/}
+						{/*				/!*<td>{tt.time}</td>*!/*/}
+						{/*				<td>{tt.time.link ? <a href={tt.time.link}>{tt.time.linkname}</a> : <span>N/A</span>}</td>*/}
+						{/*			</tr>)*/}
+						{/*	}*/}
+						{/*</table>*/}
 						<Row style={{marginTop: "30px", minWidth: "500px"}}>
 							<Col>
 								<Row style={{fontSize: "12px"}}>Data upkeep by</Row>
 								{["DTalmer", "Benjo411", "Wuyah404", "Sarah_Digitally", "theVirtual", "kwlski01"].map(p => <Row style={{fontSize: "10px"}}>- {p}</Row>)}
 							</Col>
-							{/*<Col>*/}
-							{/*	<p style={{fontSize: "12px"}}>Website by</p>*/}
-							{/*	<p style={{fontSize: "10px"}}>- kwlski</p>*/}
-							{/*</Col>*/}
+							<Col>
+								<p style={{fontSize: "12px"}}>Remarks</p>
+								<p style={{fontSize: "10px"}}>This data is put in manually, and may contain errors</p>
+								<p style={{fontSize: "10px"}}>Only A and S class times for now</p>
+								<p style={{fontSize: "10px"}}>To submit corrections and/or new data, contact one of the data maintainers</p>
+								<p style={{fontSize: "10px"}}>To submit feedback for the website or additions, please contact kwlski#5010 in discord</p>
+							</Col>
 						</Row>
 					</>
 				}
